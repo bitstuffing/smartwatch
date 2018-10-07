077823830184a31acdaa3183b7bdaf29b6c1f788
bit <harddevelop@github.com>
2018-10-07 17:10:47 +0200

    fixed hours (there are 12 regions, not like was put, 60)

--- scripts/clock.py
+++ scripts/clock.py
@@ -15,7 +15,7 @@ class Clock():
         # Since tick is always between 0 and 60, (tick / 60.0) will always be between 0.0 and 1.0
         # The (tick / 60.0) lets us break up the range between 0 and 2 * math.pi into 60 increments.
         x = math.cos(2 * math.pi * (tick / 60.0))
-        y = -1 * math.sin(2 * math.pi * (tick / 60.0)) # "-1 *" because in Pygame, the y coordinates increase going down (the opposite of how they normally go in mathematics)
+        y = -1 * math.sin(2 * math.pi * (tick / 60.0)) # "-1 *" the y coordinates increase going down (the opposite of how they normally go in mathematics)
 
         # sin() and cos() return a number between -1.0 and 1.0, so multiply to stretch it out.
         x *= radio
@@ -30,7 +30,7 @@ class Clock():
 
     def displayClock(self,screen,hours=0,minutes=0,seconds=0,ratio=32,originx=64,originy=32):
         hours = hours % 12
-        x, y = self.getTickPosition(tick=hours,radio=int(ratio/2),originx=originx,originy=originy)
+        x, y = self.getTickPosition(tick=int(hours*5),radio=int(ratio/2),originx=originx,originy=originy)
         x2, y2 = self.getTickPosition(tick=minutes,radio=int(ratio*2/3),originx=originx,originy=originy)
         x3, y3 = self.getTickPosition(tick=seconds,radio=ratio,originx=originx,originy=originy)
         screen.draw_circle(originx,originy,ratio)
